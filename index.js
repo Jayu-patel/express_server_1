0e3b27aa1cfa35d019bac48b3e3b1db53a05b9d8
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-11-20 21:09:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const app = express()
 const cors = require('cors')
 const product = require('./product')
 
-app.use(cors(
-    {
-        allowedHeaders: "*",
-        allowMethods: "*",
-        origin: "*"
-    }
-))
+app.use(cors())
 app.use(express.json())
 
 app.post('/add',async(req,res)=>{
