703a5fac49f961f7b7bd9e91d3e3a4b57c691fe8
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-11-20 20:43:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const cors = require('cors')
 const product = require('./product')
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: 'http://localhost:5173'
+    }
+))
 app.use(express.json())
 
 app.post('/add',async(req,res)=>{
